56f47941deea62e18a26b373db7036cbaaaa6284
Algety <algety@ukr.net>
2025-06-12 21:05:07 +0300

    Services on the home page done

--- home/admin.py
+++ home/admin.py
@@ -5,7 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(ContentBlock)
 class ContentBlockAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'content', 'section', 'publish')
+    list_display = ('title', 'content', 'section', 'order', 'publish')
+    list_editable = ('section', 'order', 'publish')
     search_fields = ['title','section','publish']
     list_filter = ('section','publish')
     summernote_fields = ('content',)
+    ordering = ('order',)
--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_summernote.fields import SummernoteTextField
+from cloudinary.models import CloudinaryField
 
 SECTION = [
         ("instructors", "Our Team"),
@@ -13,6 +14,12 @@ class ContentBlock(models.Model):
     # content = SummernoteTextField()
     section = models.CharField(max_length=20, choices=SECTION, default="services")
     publish = models.BooleanField(default=True)
+    block_image = CloudinaryField('image', blank=True, null=True)
+    order = models.PositiveIntegerField(default=0)  # New field for sorting
+
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
         return f"{self.title} ({self.get_section_display()})"
