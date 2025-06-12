a8094c2b24ded4e18b77aabb536db7bbadbd7f1e
Algety <algety@ukr.net>
2025-06-12 15:20:19 +0300

    Wsgi changed

--- driving_school/wsgi.py
+++ driving_school/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driving_school.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'driving_school_django.settings')
 
 application = get_wsgi_application()
